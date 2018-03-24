952c80c2679c7957e3eabbce4b25a6ebf70f0eee
Murtaza Patrawala <murtazaforuall@yahoo.in>
2018-03-24 23:10:50 +0530

    Basic Structure

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask,request,render_template,flash,redirect,url_for,session,logging
+from flask_mysqldb import MySQL
+from wtforms import Form ,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
+app=Flask(__name__)
+
+
+
+
+
+if __name__ =='__main__':
+	app.secret_key="secret123"
+	app.run(debug=True)
