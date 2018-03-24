dd81bd08ebff7c8d7dc655badcd9483b71250dab
Murtaza Patrawala <murtazaforuall@yahoo.in>
2018-03-25 01:37:06 +0530

    Registration Page added

--- main.py
+++ main.py
@@ -6,6 +6,50 @@ from functools import wraps
 
 app=Flask(__name__)
 
+#config MySQL
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='books_users'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+#initialize MYSQL
+mysql=MySQL(app)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+#register form class
+class RegisterForm(Form):
+	name=StringField('Name',[validators.Length(min=1,max=50)],render_kw={"placeholder": "Enter your Name"})
+	username=StringField('Username',[validators.Length(min=4,max=25)],render_kw={"placeholder": "Enter your username"})
+	email=StringField('Email',[validators.Length(min=6,max=50)],render_kw={"placeholder": "Enter your email"})
+	password=PasswordField('Password',[
+		validators.DataRequired(),
+		validators.EqualTo('confirm',message='Password do not match'),
+	],render_kw={"placeholder": "Enter your Password"})
+	confirm=PasswordField('Confirm Password',render_kw={"placeholder": "Confirm your Password"})
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+	form=RegisterForm(request.form)
+	if(request.method=='POST' and form.validate()):
+		cur=mysql.connection.cursor()
+		name=form.name.data
+		username=form.username.data
+		email=form.email.data
+		password=sha256_crypt.encrypt((str(form.password.data)))
+		cur.execute("INSERT INTO users(name,email,username,password) VALUES(%s,%s,%s,%s)",[name,email,username,password])
+		mysql.connection.commit()
+		cur.close()
+
+		flash('You are now registered and can LOG IN','success')
+
+		return redirect(url_for('register'))
+
+	return render_template('register.html',form=form)
+
 
 
 
